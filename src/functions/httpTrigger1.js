c874eb4c9832ec4c5eb2033b734af9e60030a6e9
JoelGodinez <joel.godinez@ict-campus.net>
2025-06-06 14:28:03 +0200

    Test

--- src/functions/httpTrigger1.js
+++ src/functions/httpTrigger1.js
@@ -1,13 +1,8 @@
 const { app,input } = require('@azure/functions');
 
 const cosmosInput = input.cosmosDB({
-<<<<<<< HEAD
     databaseName: 'TestDB',
     containerName: 'TestItems',
-=======
-    databaseName: 'DemoDatabase',
-    containerName: 'Items',
->>>>>>> 17fca5aa9a04ce3a29ca31d5a6ff076a1a5dee2e
     connection: 'CosmosDB',
     sqlQuery: "select * from c"
 });
